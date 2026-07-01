c2b9c0b62fdf798fe132d4e166973d925bac5ada
paritosh0604 <129216932+paritosh0604@users.noreply.github.com>
2026-07-01 21:09:41 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i <= nums.length-2; i=i+2){
+            if(nums[i]==nums[i+1])
+            continue;
+            else
+            return nums[i];
+        }
+        return nums[nums.length-1];
+    }
+}
